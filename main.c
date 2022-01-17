e2a6013ec0caadf84f91a07afe46f52c341d0eaf
dalbinmoon <97721722+dalbinmoon@users.noreply.github.com>
2022-01-17 22:34:33 +0900

    HW 2 Code

---
+++ main.c
@@ -0,0 +1,31 @@
+/*
+2020_03_10 21700242 Sun-Been Moon
+Numerical Analysis - HW2
+*/
+
+#include "basicHeader.h"
+#include "UserDefine.h" 
+#include "Sol_NonLinearEqns.h"
+#include "Sol_TranserFunc.h"
+#include "Linear_Algebra.h"
+#include "Interpolation.h"
+
+void main() {
+
+	/*******************************
+	HW2 - problem1
+	*******************************/
+
+	HW2_1();
+
+	/*****************************
+	HW2 - problem 2
+	*****************************/
+
+	//InterPolation(LAGRANGE);
+
+	//InterPolation(NEWTON_POL);
+
+	system("pause");
+
+}
